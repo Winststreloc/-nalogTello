e3e42c3e65c4c4555a69eff59b0c764215c31cba
Winststreloc <goloologo@gmail.com>
2023-11-13 20:08:12 +0300

    some change

--- AnalogTrello/AnalogTrelloBE/Dto/UserDto.cs
+++ AnalogTrello/AnalogTrelloBE/Dto/UserDto.cs
@@ -3,6 +3,6 @@
 public class UserDto
 {
     public string UserName { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
     public string Email { get; set; }
 }
